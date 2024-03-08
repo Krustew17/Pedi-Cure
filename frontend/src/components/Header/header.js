9d98e4c98332651d22887df371bbd5313ab59158
dimitar petrov <dimitar.petroww1811@gmail.com>
2024-03-08 19:57:42 +0200

    Improve Header

--- frontend/src/components/Header/header.js
+++ frontend/src/components/Header/header.js
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { MdExpandMore } from "react-icons/md";
 
 import { usePathname } from "next/navigation";
 
@@ -15,13 +16,23 @@ export default function Header() {
         <header className={`${styles.header} ${isHomePage ? styles.headerHome : ''}`}>
             <Link className={styles.logo} href="/">
                 <Image src="/logo.png" width={40} height={55} alt="logo" />
-                Pedi-Cure               
+                Pedi-Cure
             </Link>
             <nav className={styles.navigation}>
-                <ul>
+                <ul className={styles.navigationList}>
                     <li>За нас</li>
                     <li>Контакти</li>
-                    <li>Профилактика на проблемни стъпала</li>
+                    <li className={styles.itemDropdown}>
+                        Профилактика на проблемни стъпала
+                        <MdExpandMore />
+                        <ul className={styles.itemDropdownSubmenu}>
+                            <li>Гъбички</li>
+                            <li>Впити нокти</li>
+                            <li>Хиперкератоза</li>
+                            <li>Кератодермия</li>
+                            <li>Онихолиза</li>
+                        </ul>
+                    </li>
                 </ul>
             </nav>
         </header>
